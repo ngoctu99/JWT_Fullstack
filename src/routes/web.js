ee7b73059b550f5cc2ac44947bcc5b3b38146149
inf4153 <inf4153@hs-worms.de>
2025-03-24 09:24:09 +0100

    feature creating user completed

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { homePage } from "../controller/homePageController.js";
-import { userPage } from "../controller/userPageController.js";
+import { createNewUser, userPage } from "../controller/userPageController.js";
 
 const router = express.Router();
 
 
 router.get('/', homePage)
 router.get('/user', userPage)
+
+router.post('/users/create-user', createNewUser)
 export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import viewEngine from "./config/viewEngine.js";
 import router from "./routes/web.js";
 import dotenv from "dotenv";
-
+import bodyParser from "body-parser";
 dotenv.config();
 
 
@@ -11,6 +11,9 @@ const port = process.env.PORT || 5000;
 const host_name = process.env.HOST_NAME || "localhost";
 
 viewEngine(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
 
 app.use('/', router);
 
